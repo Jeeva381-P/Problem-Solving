d16ff629b86c6c620f201920ee1281b72a10c26e
Jeevananthan P <69036557+Jeeva381-P@users.noreply.github.com>
2022-11-15 23:14:34 +0530

    Update reverse_array.cpp

--- reverse_array.cpp
+++ reverse_array.cpp
@@ -26,35 +26,3 @@ int main() {
 	}
 	return 0;
 }
-
-
-
-#include <iostream>
-using namespace std;
-
-void reverse (int arr[], int N) {
-    int start = 0;
-    int end = N-1;
-    while (start < end) {
-        int temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
-        start++; end--;
-    }
-}
-
-int main() {
-	int N;
-	cin >> N;
-	int A[N];
-	for (int i = 0; i < N; i++) {
-	    cin >> A[i];
-	}
-	reverse(A,N);
-	for (int i = 0; i < N; i++) {
-	    cout << A[i] << " ";
-	}
-	return 0;
-}
-
-
